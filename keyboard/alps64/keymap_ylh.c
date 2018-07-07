256120cbf7b024629bc2bd421b4b650dd5b384a5
Ron Huang <ron@hng.tw>
2018-07-07 17:51:52 +0800

    Make left ctrl as fn0

--- keyboard/alps64/keymap_ylh.c
+++ keyboard/alps64/keymap_ylh.c
@@ -19,7 +19,7 @@ const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS] PROGMEM = {
         TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,BSLS, \
         LCTL,A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,ENT, \
         LSFT,Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,RSFT, \
-        LCTL,LALT,LGUI,          SPC,                          RGUI,APP, FN0),
+        FN0 ,LALT,LGUI,          SPC,                          RGUI,APP, FN0),
 
     /* HHKB mode
      * ,-----------------------------------------------------------.
